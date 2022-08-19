b9851783e5400900a5de8ad43cb6f407f2cd1e4d
Patrick von Platen <patrick.v.platen@gmail.com>
2022-08-19 17:14:29 +0000

    fix to numpy

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -266,7 +266,7 @@ def main():
 
                         x_samples_ddim = model.decode_first_stage(samples_ddim)
                         x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
-                        x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1)
+                        x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
 
                         x_image = x_samples_ddim
                         safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
