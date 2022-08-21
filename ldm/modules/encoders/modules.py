dad97f4b7ef9d995b598fa18c75e60b7e53bc616
bes <belbes122@yandex.ru>
2022-08-21 15:58:17 +0300

    remove deprecated code

--- ldm/modules/encoders/modules.py
+++ ldm/modules/encoders/modules.py
@@ -142,7 +142,6 @@ class SpatialRescaler(Module):
 
 class FrozenCLIPEmbedder(AbstractEncoder):
     """Uses the CLIP transformer encoder for text (from Hugging Face)"""
-    # def __init__(self, version="openai/clip-vit-large-patch14", device="cuda", max_length=77):
     def __init__(self, version="openai/clip-vit-large-patch14", max_length=77):
         super().__init__()
         self.tokenizer = CLIPTokenizer.from_pretrained(version)
