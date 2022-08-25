f64ed38e2bdf8752140f0fd7459f51a1c1e8162f
hlky <106811348+hlky@users.noreply.github.com>
2022-08-25 14:31:21 +0100

    Update webuildm.cmd

--- webuildm.cmd
+++ webuildm.cmd
@@ -2,7 +2,12 @@
 call C:\ProgramData\miniconda3\Scripts\activate.bat
 call conda env create -f environment.yaml
 call C:\ProgramData\miniconda3\Scripts\activate.bat ldm
-python "%CD%"\scripts\webui.py
+python "%CD%"\scripts\relauncher.py
 
 :PROMPT
-python scripts/webui.py
+set SETUPTOOLS_USE_DISTUTILS=stdlib
+IF EXIST "models\ldm\stable-diffusion-v1\model.ckpt" (
+  python scripts/relauncher.py
+) ELSE (
+  ECHO Your model file does not exist! Place it in 'models\ldm\stable-diffusion-v1' with the name 'model.ckpt'.
+)
