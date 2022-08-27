8a79b999fa294cdfff6680b4175dc6034adbe353
hlky <106811348+hlky@users.noreply.github.com>
2022-08-27 02:13:38 +0100

    Update webuildm.cmd

--- webuildm.cmd
+++ webuildm.cmd
@@ -1,6 +1,7 @@
 @echo off
 call C:\ProgramData\miniconda3\Scripts\activate.bat
 call conda env create -f environment.yaml
+call conda env update --file environment.yaml --prune
 call C:\ProgramData\miniconda3\Scripts\activate.bat ldm
 python "%CD%"\scripts\relauncher.py
 
