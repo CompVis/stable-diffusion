fab9e1a42369edd676cc0c07509ce814d258dffe
Andy Pilate <cubox@cubox.dev>
2022-08-28 17:11:24 +0200

    Fix wrong help message

--- scripts/dream.py
+++ scripts/dream.py
@@ -409,7 +409,7 @@ def create_argv_parser():
         '--gfpgan_bg_upsampler',
         type=str,
         default='realesrgan',
-        help='Background upsampler. Default: None. Options: realesrgan, none. Only used if --gfpgan is specified',
+        help='Background upsampler. Default: realesrgan. Options: realesrgan, none. Only used if --gfpgan is specified',
     )
     parser.add_argument(
         '--gfpgan_bg_tile',
