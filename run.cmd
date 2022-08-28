6b442c05296dd350fcf7f752e17c4b4bb015a03b
ShinkoNet <10773386+ShinkoNet@users.noreply.github.com>
2022-08-28 14:45:57 +1000

    Delete run.cmd

--- run.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set SETUPTOOLS_USE_DISTUTILS=stdlib
-IF EXIST "models\ldm\stable-diffusion-v1\model.ckpt" (
-  python scripts/relauncher.py
-) ELSE (
-  ECHO Your model file does not exist! Place it in 'models\ldm\stable-diffusion-v1' with the name 'model.ckpt'.
-)
