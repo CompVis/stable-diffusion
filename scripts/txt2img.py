3c9f69e1def5e4750a7bdb9888c30dac611e13b0
Ben Firshman <ben@firshman.co.uk>
2022-08-31 13:56:44 -0700

    Make it work with plain pip

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -14,6 +14,8 @@ from pytorch_lightning import seed_everything
 from torch import autocast
 from contextlib import contextmanager, nullcontext
 
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 from ldm.util import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
