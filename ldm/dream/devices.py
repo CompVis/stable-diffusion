84c10346fb777c827df58b264a726572225a45c6
James Reynolds <magnsuviri@me.com>
2022-08-31 03:29:37 -0600

    check if torch.backends has mps before calling it

--- ldm/dream/devices.py
+++ ldm/dream/devices.py
@@ -4,7 +4,7 @@ def choose_torch_device() -> str:
     '''Convenience routine for guessing which GPU device to run model on'''
     if torch.cuda.is_available():
         return 'cuda'
-    if torch.backends.mps.is_available():
+    if hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
         return 'mps'
     return 'cpu'
 
