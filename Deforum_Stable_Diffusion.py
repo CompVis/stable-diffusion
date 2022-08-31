63d00e125f5fb755f557d86dadde60314fe2742e
enzymezoo-code <enzymezoo.art@gmail.com>
2022-08-31 15:49:32 -0500

    Prepared for dev branch

--- Deforum_Stable_Diffusion.py
+++ Deforum_Stable_Diffusion.py
@@ -78,7 +78,7 @@ if setup_environment:
     print("...setting up environment")
     all_process = [['pip', 'install', 'torch==1.11.0+cu113', 'torchvision==0.12.0+cu113', 'torchaudio==0.11.0', '--extra-index-url', 'https://download.pytorch.org/whl/cu113'],
                    ['pip', 'install', 'omegaconf==2.1.1', 'einops==0.3.0', 'pytorch-lightning==1.4.2', 'torchmetrics==0.6.0', 'torchtext==0.2.3', 'transformers==4.19.2', 'kornia==0.6'],
-                   ['git', 'clone', '-b', 'inpainting_1.0', 'https://github.com/deforum/stable-diffusion'],
+                   ['git', 'clone', '-b', 'dev', 'https://github.com/deforum/stable-diffusion'],
                    ['pip', 'install', '-e', 'git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers'],
                    ['pip', 'install', '-e', 'git+https://github.com/openai/CLIP.git@main#egg=clip'],
                    ['pip', 'install', 'accelerate', 'ftfy', 'jsonmerge', 'resize-right', 'torchdiffeq'],
