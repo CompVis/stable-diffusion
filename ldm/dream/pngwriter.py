e918cb1a8aa4f08cb552733b73f2f717e54db980
Kevin Gibbons <bakkot@gmail.com>
2022-09-02 23:36:44 -0700

    replace list delimiters in variations syntax

--- ldm/dream/pngwriter.py
+++ ldm/dream/pngwriter.py
@@ -72,7 +72,7 @@ class PromptFormatter:
         if opt.variation_amount > 0:
             switches.append(f'-v {opt.variation_amount}')
         if opt.with_variations:
-            formatted_variations = ';'.join(f'{seed},{weight}' for seed, weight in opt.with_variations)
+            formatted_variations = ','.join(f'{seed}:{weight}' for seed, weight in opt.with_variations)
             switches.append(f'-V {formatted_variations}')
         if t2i.full_precision:
             switches.append('-F')
