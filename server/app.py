f17eb44324d603ee2f17d70c185436100afaf7b6
kreas <hello@timwarren.dev>
2022-09-05 11:55:08 -0500

    Adds image generator queue and worker

--- server/app.py
+++ server/app.py
@@ -1,11 +1,17 @@
 import os
+import redis
+import json
+import uuid
+
 from flask import Flask, request, jsonify
-from txt2img import generate as generateImage
+from time import time
 from dotenv import load_dotenv
 
 load_dotenv('../.env')
 
 app = Flask(__name__)
+r = redis.Redis()
+
 
 def verify_api_key():
     """Verify that the API key is valid"""
@@ -20,30 +26,46 @@ def generate():
     """Generate an image from a prompt"""
     args = request.json
     prompt = args.get('prompt', None)
+    webhook_url = request.headers.get('X-WEBHOOK-URL', None)
+    job_id = uuid.uuid4().hex
 
-    if prompt is not None and verify_api_key():
-        results, time = generateImage(
-            prompt = prompt,
-            seed = args.get('seed', 0),
-            width = args.get('width', 512),
-            height = args.get('height', 512),
-            steps = args.get('steps', 40),
-            iterations = args.get('iterations', 1),
-            scale = args.get('scale', 7.5),
-        )
+    if not prompt:
+        return jsonify({'error': 'No prompt provided'}), 400
 
-        return jsonify(
-            data=results,
-            meta={
-                'total_time': round(time, 2),
-            }
-        )
-    else:
-        return jsonify({'error': 'No prompt provided.'}), 406
+    if not webhook_url:
+        return jsonify({'error': 'No webhook URL provided'}), 400
+
+    if not verify_api_key():
+        return jsonify({'error': 'Invalid API key'}), 401
+
+    params = {
+        'job_id': job_id,
+        'webhook_url': webhook_url,
+        'seed': args.get('seed', 0),
+        'prompt': args.get('prompt', None),
+        'width': args.get('width', 512),
+        'height': args.get('height', 512),
+        'steps': args.get('steps', 40),
+        'iterations': args.get('iterations', 1),
+        'scale': args.get('scale', 7.5),
+    }
+
+    r.rpush('generate_images', json.dumps(obj=params))
+    
+    return jsonify({
+        'job_id': job_id,
+    })
+
+
+@app.route('/webhook', methods=['POST'])
+def test_webhook():
+    print(request.json)
+    return 'OK!'
 
 
 @app.route('/', methods=['GET'])
 def heartbeat():
     return 'OK!'
 
-app.run(host='0.0.0.0', port=3001, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3001, debug=True)
---
+++ server/worker.py
@@ -0,0 +1,45 @@
+import redis
+import time
+import json
+from txt2img import generate as generate_image
+from dotenv import load_dotenv
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import requests
+
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
+
+r = redis.Redis()
+
+def create_image(prompt, args):
+    """Create image and upload to S3"""
+    webhook = args.get('webhook_url')
+
+    if not webhook:
+        return
+        
+    try:
+        images, time = generate_image(
+            prompt = prompt,
+            seed = args.get('seed', 0),
+            width = args.get('width', 512),
+            height = args.get('height', 512),
+            steps = args.get('steps', 40),
+            iterations = args.get('iterations', 1),
+            scale = args.get('scale', 7.5),
+        )
+
+        requests.post(webhook, json={ 'images': images, 'time': time })
+    except Exception as e:
+        print(e)
+
+
+while True:
+    data = r.lpop('generate_images')
+
+    if data:
+        data = json.loads(data)
+        create_image(data['prompt'], data)
+
