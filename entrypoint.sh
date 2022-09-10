b656d333de9019739daaeb85b3a14172513fcae8
Armando C. Santisbon <armando.santisbon@gmail.com>
2022-09-10 13:56:18 -0500

    Add entrypoint and update documentation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 ln -sf /data/sd-v1-4.ckpt /stable-diffusion/models/ldm/stable-diffusion-v1/model.ckpt
+cd /stable-diffusion
+conda activate ldm
 
 if [ $# -eq 0 ]; then
-    python3 /stable-diffusion/scripts/dream.py --full_precision -o /data 
+    python3 scripts/dream.py --full_precision -o /data 
 else
-    python3 /stable-diffusion/scripts/dream.py --full_precision -o /data "$@"
+    python3 scripts/dream.py --full_precision -o /data "$@"
 fi
