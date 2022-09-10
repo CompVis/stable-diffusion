52cf0e5300f40e4e52dbc9facc38ed3f5eda110f
Austin Ensminger <33635167+ensmingera@users.noreply.github.com>
2022-09-10 03:04:34 -0400

    Add fprefix argument
    Add "fprefix" argument. This argument defines the "filename prefix" for the generated samples. Sample filenames will then be: <prefix><base|grid_count>.png

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -226,6 +226,11 @@ def main():
         choices=["full", "autocast"],
         default="autocast"
     )
+    parser.add_argument(
+        "--fprefix",
+        type=str,
+        help="filename prefix for sample outputs"
+    )
     opt = parser.parse_args()
 
     if opt.laion400m:
@@ -315,7 +320,7 @@ def main():
                                 x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
                                 img = Image.fromarray(x_sample.astype(np.uint8))
                                 img = put_watermark(img, wm_encoder)
-                                img.save(os.path.join(sample_path, f"{base_count:05}.png"))
+                                img.save(os.path.join(sample_path, f"{opt.prefix or ''}{base_count:05}.png"))
                                 base_count += 1
 
                         if not opt.skip_grid:
