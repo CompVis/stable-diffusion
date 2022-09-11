442275c8c83cd3fd46d0859267338ba99f098d6d
Astropulse <Astropulse@fastmail.com>
2022-09-10 23:01:30 -0400

    Fixed PIL depreciation warning.

--- scripts/img2img.py
+++ scripts/img2img.py
@@ -81,7 +81,7 @@ def load_img(path):
     w, h = image.size
     print(f"loaded input image of size ({w}, {h}) from {path}")
     w, h = map(lambda x: x - x % 64, (w, h))  # lmao, it's 64 not 32
-    image = image.resize((w, h), resample=PIL.Image.LANCZOS)
+    image = image.resize((w, h), resample=Image.Resampling.LANCZOS)
     image = np.array(image).astype(np.float32) / 255.0
     image = image[None].transpose(0, 3, 1, 2)
     image = torch.from_numpy(image)
