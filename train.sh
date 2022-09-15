0cfa39a37359e500cbaa699ec15ebfe8f411238f
Frederik Fix <ich@derfred.com>
2022-09-15 19:46:52 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 main.py --resume model.ckpt --base ./configs/stable-diffusion/v1-finetune-4gpu.yaml --no-test --seed 25 --scale_lr False --gpus 0,1,2,3
+python3 main.py --train --resume model.ckpt --base ./configs/stable-diffusion/v1-finetune-4gpu.yaml --no-test --seed 25 --scale_lr False --gpus 0,1,2,3
