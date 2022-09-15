f9b272a7b9e97f0c434e425b41f5872a36db40aa
ArDiouscuros <72071512+ArDiouscuros@users.noreply.github.com>
2022-09-16 01:29:19 +0200

    Update to exclude GTX 16xx from autocast selection (#576)
    * Update to exclude GTX 1660/1650 from autocast selection

--- ldm/dream/devices.py
+++ ldm/dream/devices.py
@@ -13,9 +13,12 @@ def choose_torch_device() -> str:
 def choose_autocast_device(device):
     '''Returns an autocast compatible device from a torch device'''
     device_type = device.type # this returns 'mps' on M1
+    # autocast only for cuda, but GTX 16xx have issues with it
     if device_type == 'cuda':
-        return device_type,autocast
-    elif device_type == 'cpu':
-        return device_type,nullcontext
+        device_name = torch.cuda.get_device_name()
+        if 'GeForce GTX 1660' in device_name or 'GeForce GTX 1650' in device_name:
+            return device_type,nullcontext
+        else:
+            return device_type,autocast
     else:
         return 'cpu',nullcontext
