ac51ec4939395ee68f3de8849a140595990209b5
hipsterusername <kentk@hey.com>
2022-09-23 09:09:48 -0400

    Addressed merge conflicts for adding UI tooltips

---
+++ frontend/src/common/components/GuidePopover.tsx
@@ -0,0 +1,51 @@
+import {
+  Popover,
+  PopoverArrow,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverHeader,
+  Flex,
+  Box,
+} from '@chakra-ui/react';
+import { SystemState } from '../../features/system/systemSlice';
+import { useAppSelector } from '../../app/store';
+import { RootState } from '../../app/store';
+import { createSelector } from '@reduxjs/toolkit';
+import { ReactElement } from 'react';
+import {  Feature, FEATURES } from '../../app/features';
+
+type GuideProps = {
+  children: ReactElement;
+  feature: Feature;
+};
+
+const systemSelector = createSelector(
+  (state: RootState) => state.system,
+  (system: SystemState) => system.shouldDisplayGuides
+);
+
+const GuidePopover = ({ children, feature }: GuideProps) => {
+  const shouldDisplayGuides = useAppSelector(systemSelector);
+  const { text } = FEATURES[feature];
+  return shouldDisplayGuides ? (
+    <Popover trigger={'hover'}>
+      <PopoverTrigger>
+        <Box>{children}</Box>
+      </PopoverTrigger>
+      <PopoverContent
+        maxWidth="400px"
+        onClick={(e) => e.preventDefault()}
+        cursor={'initial'}
+      >
+        <PopoverArrow />
+        <Flex alignItems={'center'} gap={2} p={4}>
+          {text}
+        </Flex>
+      </PopoverContent>
+    </Popover>
+  ) : (
+    <></>
+  );
+};
+
+export default GuidePopover;
--- frontend/src/features/system/SettingsModal.tsx
+++ frontend/src/features/system/SettingsModal.tsx
@@ -20,6 +20,7 @@ import { useAppDispatch, useAppSelector } from '../../app/store';
 import {
   setShouldConfirmOnDelete,
   setShouldDisplayInProgress,
+  setShouldDisplayGuides,
   SystemState,
 } from './systemSlice';
 import { RootState } from '../../app/store';
@@ -31,8 +32,8 @@ import { cloneElement, ReactElement } from 'react';
 const systemSelector = createSelector(
   (state: RootState) => state.system,
   (system: SystemState) => {
-    const { shouldDisplayInProgress, shouldConfirmOnDelete } = system;
-    return { shouldDisplayInProgress, shouldConfirmOnDelete };
+    const { shouldDisplayInProgress, shouldConfirmOnDelete, shouldDisplayGuides } = system;
+    return { shouldDisplayInProgress, shouldConfirmOnDelete, shouldDisplayGuides };
   },
   {
     memoizeOptions: { resultEqualityCheck: isEqual },
@@ -63,7 +64,7 @@ const SettingsModal = ({ children }: SettingsModalProps) => {
     onClose: onRefreshModalClose,
   } = useDisclosure();
 
-  const { shouldDisplayInProgress, shouldConfirmOnDelete } =
+  const { shouldDisplayInProgress, shouldConfirmOnDelete, shouldDisplayGuides } =
     useAppSelector(systemSelector);
 
   const dispatch = useAppDispatch();
@@ -112,6 +113,19 @@ const SettingsModal = ({ children }: SettingsModalProps) => {
                     isChecked={shouldConfirmOnDelete}
                     onChange={(e) =>
                       dispatch(setShouldConfirmOnDelete(e.target.checked))
+                    }
+                  />
+                </HStack>
+              </FormControl>
+              <FormControl>
+                <HStack>
+                  <FormLabel marginBottom={1}>
+                    Display help guides in configuration menus
+                  </FormLabel>
+                  <Switch
+                    isChecked={shouldDisplayGuides}
+                    onChange={(e) =>
+                      dispatch(setShouldDisplayGuides(e.target.checked))
                     }
                   />
                 </HStack>
