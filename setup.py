88dcd163636ffc6fa6a615d8f4b2f242f517632e
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2022-09-27 17:02:50 -0600

    remove git repos from install

--- setup.py
+++ setup.py
@@ -26,10 +26,7 @@ setup(
         "transformers",
         "torchmetrics",
         "kornia",
-        "natsort",
-        "-e git+https://github.com/w4ffl35/taming-transformers.git#egg=taming-transformers",
-        "-e git+https://github.com/w4ffl35/CLIP.git#egg=clip",
-        "-e git+https://github.com/albumentations-team/albumentations#egg=albumentations",
+        "natsort"
     ],
     include_package_data=True,
     extras_require={'dev': ['pytest']},
