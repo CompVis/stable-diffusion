7f1f173c118eb072604f8ba3bcc39d1b9bfd42b9
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2022-09-27 18:16:05 -0600

    adds sd runner

---
+++ sdrunner.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import queue
+
+
+class StableDiffusionConnectionManager:
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize all connections and workers
+        """
+        # create queues
+        self.request_queue = kwargs.get("request_queue", queue.SimpleQueue())
+        self.response_queue = kwargs.get("response_queue", queue.SimpleQueue())
+
+        # create request client
+        print("creating request worker...")
+        self.request_worker = StableDiffusionRequestQueueWorker(
+            port=50006,
+            pid=kwargs.get("pid"),
+        )
+
+if __name__ == "__main__":
+    # get pid from command line
+    pid = sys.argv[2]
+    # clear sys.argv
+    sys.argv = sys.argv[:1]
+    StableDiffusionConnectionManager(pid=0)
