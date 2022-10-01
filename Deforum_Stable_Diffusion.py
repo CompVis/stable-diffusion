ad37cc3c3c90b8f8edafe3725e048c2b73167dc4
kabachuha <artemkhrapov2001@yandex.ru>
2022-10-01 15:26:22 +0300

    Add stablility.ai license in a collapsable cell

--- Deforum_Stable_Diffusion.py
+++ Deforum_Stable_Diffusion.py
@@ -7,13 +7,25 @@
 [Stable Diffusion](https://github.com/CompVis/stable-diffusion) by Robin Rombach, Andreas Blattmann, Dominik Lorenz, Patrick Esser, Björn Ommer and the [Stability.ai](https://stability.ai/) Team. [K Diffusion](https://github.com/crowsonkb/k-diffusion) by [Katherine Crowson](https://twitter.com/RiversHaveWings). You need to get the ckpt file and put it on your Google Drive first to use this. It can be downloaded from [HuggingFace](https://huggingface.co/CompVis/stable-diffusion).
 
 Notebook by [deforum](https://discord.gg/upmXXsrwZc)
-"""
 
 # %%
 # !! {"metadata":{
 # !!   "id": "T4knibRpAQ06"
 # !! }}
 """
+By using this Notebook, you agree to the following Terms of Use, and license:
+
+**Stablity.AI Model Terms of Use**
+
+This model is open access and available to all, with a CreativeML OpenRAIL-M license further specifying rights and usage.
+
+The CreativeML OpenRAIL License specifies:
+
+You can't use the model to deliberately produce nor share illegal or harmful outputs or content
+CompVis claims no rights on the outputs you generate, you are free to use them and are accountable for their use which must not go against the provisions set in the license
+You may re-distribute the weights and use the model commercially and/or as a service. If you do, please be aware you have to include the same use restrictions as the ones in the license and share a copy of the CreativeML OpenRAIL-M to all your users (please read the license entirely and carefully)
+Please read the full license here: https://huggingface.co/spaces/CompVis/stable-diffusion-license
+
 # Setup
 """
 
