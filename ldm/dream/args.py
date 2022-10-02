8ba5e385ec979f4728c0995660e9762a478a11cd
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2022-10-02 15:56:57 +1100

    Fixes #877

--- ldm/dream/args.py
+++ ldm/dream/args.py
@@ -227,8 +227,8 @@ class Args(object):
         # 2. However, they come out of the CLI (and probably web) with the keyword "with_variations" and
         #    in broken-out form. Variation (1) should be changed to comply with (2)
         if a['with_variations']:
-            formatted_variations = ','.join(f'{seed}:{weight}' for seed, weight in (a["variations"]))
-            switches.append(f'-V {a["formatted_variations"]}')
+            formatted_variations = ','.join(f'{seed}:{weight}' for seed, weight in (a["with_variations"]))
+            switches.append(f'-V {formatted_variations}')
         if 'variations' in a:
             switches.append(f'-V {a["variations"]}')
         return ' '.join(switches)
