6c0dd9b5ef55c15c1263524798ac5c0e6cc5e29c
CapableWeb <capableweb@domain.com>
2022-10-12 20:05:55 +0200

    Add back old `dream.py` as `legacy_api.py`
    This commit "reverts" the new API changes by extracting the old
    functionality into new files.
    
    The work is based on the commit `803a51d5adca7e6e28491fc414fd3937bee7cb79`
    
    PngWriter regained PromptFormatter as old server used that.
    
    `server_legacy.py` is the old server that `dream.py` used.
    
    Finally `legacy_api.py` is what `dream.py` used to be at the mentioned
    commit.
    
    One manually run test has been added in order to be able to test
    compatibility with the old API, currently just testing that the API
    endpoint works the same way + the image hash is the same as it used to
    be before.

--- ldm/invoke/pngwriter.py
+++ ldm/invoke/pngwriter.py
@@ -66,3 +66,43 @@ def write_metadata(img_path:str, meta:dict):
     info = PngImagePlugin.PngInfo()
     info.add_text('sd-metadata', json.dumps(meta))
     im.save(img_path,'PNG',pnginfo=info)
+
+class PromptFormatter:
+    def __init__(self, t2i, opt):
+        self.t2i = t2i
+        self.opt = opt
+
+    # note: the t2i object should provide all these values.
+    # there should be no need to or against opt values
+    def normalize_prompt(self):
+        """Normalize the prompt and switches"""
+        t2i = self.t2i
+        opt = self.opt
+
+        switches = list()
+        switches.append(f'"{opt.prompt}"')
+        switches.append(f'-s{opt.steps        or t2i.steps}')
+        switches.append(f'-W{opt.width        or t2i.width}')
+        switches.append(f'-H{opt.height       or t2i.height}')
+        switches.append(f'-C{opt.cfg_scale    or t2i.cfg_scale}')
+        switches.append(f'-A{opt.sampler_name or t2i.sampler_name}')
+# to do: put model name into the t2i object
+#        switches.append(f'--model{t2i.model_name}')
+        if opt.seamless or t2i.seamless:
+            switches.append(f'--seamless')
+        if opt.init_img:
+            switches.append(f'-I{opt.init_img}')
+        if opt.fit:
+            switches.append(f'--fit')
+        if opt.strength and opt.init_img is not None:
+            switches.append(f'-f{opt.strength or t2i.strength}')
+        if opt.gfpgan_strength:
+            switches.append(f'-G{opt.gfpgan_strength}')
+        if opt.upscale:
+            switches.append(f'-U {" ".join([str(u) for u in opt.upscale])}')
+        if opt.variation_amount > 0:
+            switches.append(f'-v{opt.variation_amount}')
+        if opt.with_variations:
+            formatted_variations = ','.join(f'{seed}:{weight}' for seed, weight in opt.with_variations)
+            switches.append(f'-V{formatted_variations}')
+        return ' '.join(switches)
