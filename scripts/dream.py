82b7c118c4a0444b9fc6889551f5324b2529c6cd
db3000 <email@danielbeer.com>
2022-10-13 01:06:10 -0400

    Forward dream.py to invoke.py using the same interpreter, add deprecation warning

--- scripts/dream.py
+++ scripts/dream.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 # Copyright (c) 2022 Lincoln D. Stein (https://github.com/lstein)
 
-import sys
-import os.path
+import warnings
+import invoke
 
-script_path = sys.argv[0]
-script_args = sys.argv[1:]
-script_dir,script_name = os.path.split(script_path)
-script_dest = os.path.join(script_dir,'invoke.py')
-os.execlp('python3','python3',script_dest,*script_args)
-
+if __name__ == '__main__':
+    warnings.warn("dream.py is deprecated, please run invoke.py instead",
+                  DeprecationWarning)
+    invoke.main()
