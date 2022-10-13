7f491fd2d27f9743b489c29353e11143814c0487
db3000 <email@danielbeer.com>
2022-10-13 08:28:08 -0400

    Reword deprecation warning for dream.py

--- scripts/dream.py
+++ scripts/dream.py
@@ -5,6 +5,7 @@ import warnings
 import invoke
 
 if __name__ == '__main__':
-    warnings.warn("dream.py is deprecated, please run invoke.py instead",
+    warnings.warn("dream.py is being deprecated, please run invoke.py for the "
+                  "new UI/API or legacy_api.py for the old API",
                   DeprecationWarning)
     invoke.main()
