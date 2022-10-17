e7c9ad3849c55fc4908f0650c8a609b6b251dcf6
rohitgr7 <rohitgr1998@gmail.com>
2022-10-17 20:07:37 +0530

    fix

--- ldm/modules/encoders/modules.py
+++ ldm/modules/encoders/modules.py
@@ -136,10 +136,11 @@ class SpatialRescaler(nn.Module):
 
 class FrozenCLIPEmbedder(AbstractEncoder):
     """Uses the CLIP transformer encoder for text (from Hugging Face)"""
-    def __init__(self, version="openai/clip-vit-large-patch14", max_length=77):
+    def __init__(self, version="openai/clip-vit-large-patch14", device='cuda', max_length=77):
         super().__init__()
         self.tokenizer = CLIPTokenizer.from_pretrained(version)
         self.transformer = CLIPTextModel.from_pretrained(version)
+        self.device = device
         self.max_length = max_length
         self.freeze()
 
@@ -151,7 +152,7 @@ class FrozenCLIPEmbedder(AbstractEncoder):
     def forward(self, text):
         batch_encoding = self.tokenizer(text, truncation=True, max_length=self.max_length, return_length=True,
                                         return_overflowing_tokens=False, padding="max_length", return_tensors="pt")
-        tokens = batch_encoding["input_ids"]
+        tokens = batch_encoding["input_ids"].to(self.device)
         outputs = self.transformer(input_ids=tokens)
 
         z = outputs.last_hidden_state
