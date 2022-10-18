90d37eac034592cc3aed5a15a98971801b21988e
Lincoln Stein <lincoln.stein@gmail.com>
2022-10-18 16:00:59 -0400

    update requirements to address #1149

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='invoke-ai',
-    version='2.0.0',
+    version='2.0.2',
     description='',
     packages=find_packages(),
     install_requires=[
