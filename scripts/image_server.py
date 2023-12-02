aa8a20ae769ac9d65f7032746f117cac624d1ab6
Astropulse <Astropulse@fastmail.com>
2023-12-02 11:46:52 -0500

    Allow k-centroid to resize multiple cels

--- scripts/image_server.py
+++ scripts/image_server.py
@@ -700,18 +700,18 @@ def kCentroid(image, width, height, centroids):
 
     # Iterate over each tile in the downscaled image
     for x, y in product(range(width), range(height)):
-            # Crop the tile from the original image
-            tile = image.crop((x*wFactor, y*hFactor, (x*wFactor)+wFactor, (y*hFactor)+hFactor))
+        # Crop the tile from the original image
+        tile = image.crop((x*wFactor, y*hFactor, (x*wFactor)+wFactor, (y*hFactor)+hFactor))
 
-            # Quantize the colors of the tile using k-means clustering
-            tile = tile.quantize(colors=centroids, method=1, kmeans=centroids).convert("RGB")
+        # Quantize the colors of the tile using k-means clustering
+        tile = tile.quantize(colors=centroids, method=1, kmeans=centroids).convert("RGB")
 
-            # Get the color counts and find the most common color
-            color_counts = tile.getcolors()
-            most_common_color = max(color_counts, key=lambda x: x[0])[1]
+        # Get the color counts and find the most common color
+        color_counts = tile.getcolors()
+        most_common_color = max(color_counts, key=lambda x: x[0])[1]
 
-            # Assign the most common color to the corresponding pixel in the downscaled image
-            downscaled[y, x, :] = most_common_color
+        # Assign the most common color to the corresponding pixel in the downscaled image
+        downscaled[y, x, :] = most_common_color
 
     return Image.fromarray(downscaled, mode='RGB')
 
@@ -1057,17 +1057,28 @@ def rembg(modelpath, numFiles):
                     play("batch.wav")
     rprint(f"[#c4f129]Removed [#48a971]{len(files)}[#c4f129] backgrounds in [#48a971]{round(time.time()-timer, 2)}[#c4f129] seconds")
 
-def kCentroidVerbose(width, height, centroids):
-    # Check if the input file exists and open it
-    assert os.path.isfile("temp/input.png")
-    init_img = Image.open("temp/input.png")
+def kCentroidVerbose(width, height, centroids, numFiles):
+    timer = time.time()
+    init_img = Image.open("temp/input1.png")
+    files = []
 
     rprint(f"\n[#48a971]K-Centroid downscaling[white] from [#48a971]{init_img.width}[white]x[#48a971]{init_img.height}[white] to [#48a971]{width}[white]x[#48a971]{height}[white] with [#48a971]{centroids}[white] centroids")
 
     # Perform k-centroid downscaling and save the image
-    for _ in clbar(range(1), name = "Processed", unit = "image", prefixwidth = 12, suffixwidth = 28):
-        kCentroid(init_img, int(width), int(height), int(centroids)).save("temp/temp.png")
-    play("batch.wav")
+    # Create a list of file paths
+    for n in range(numFiles):
+        files.append(f"temp/input{n+1}.png")
+
+    for file in clbar(files, name = "Processed", unit = "image", prefixwidth = 12, suffixwidth = 28):
+        img = Image.open(file).convert('RGB')
+        kCentroid(img, int(width), int(height), int(centroids)).save(file)
+
+        if file != files[-1]:
+            play("iteration.wav")
+        else:
+            play("batch.wav")
+
+    rprint(f"\n[#c4f129]Resized in [#48a971]{round(time.time()-timer, 2)} [#c4f129]seconds")
 
 def render(modelFS, modelPV, samples_ddim, i, device, H, W, pixelSize, pixelvae, tilingX, tilingY, loraFiles, post):
     if pixelvae == "true":
@@ -1960,8 +1971,8 @@ async def server(websocket):
             await websocket.send("running kcentroid")
             try:
                 # Extract parameters from the message
-                width, height, centroids = searchString(message, "dwidth", "dheight", "dcentroids", "end")
-                kCentroidVerbose(int(width), int(height), int(centroids))
+                width, height, centroids, numFiles = searchString(message, "dwidth", "dheight", "dcentroids", "dnumfiles", "end")
+                kCentroidVerbose(int(width), int(height), int(centroids), int(numFiles))
                 await websocket.send("returning kcentroid")
             except Exception as e: 
                 rprint(f"\n[#ab333d]ERROR:\n{traceback.format_exc()}")
