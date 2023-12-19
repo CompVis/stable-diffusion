86e59163bd92821beedc3595569c63ab4a780907
Astropulse <Astropulse@fastmail.com>
2023-12-19 10:00:56 -0500

    Fix sampling

--- scripts/ddpm.py
+++ scripts/ddpm.py
@@ -837,7 +837,7 @@ class UNet(DDPM):
     ):
         b, *_, device = *x.shape, x.device
 
-        if unconditional_conditioning is None or unconditional_guidance_scale == 1.0 or (index % min(5, max(1, round(total/10))) > 0 and (index >= min(20, max(1, total)/3))):
+        if unconditional_conditioning is None or unconditional_guidance_scale == 1.0 or (index % min(5, max(1, round(total/10))) > 0 and (index >= min(20, max(5, total/3)))):
             e_t = self.apply_model(x, t, c)
         else:
             x_in = torch.cat([x] * 2)
@@ -912,7 +912,7 @@ class UNet(DDPM):
 
             s_i = sigma_hat * s_in
 
-            if unconditional_conditioning is None or unconditional_guidance_scale == 1.0 or (i % min(5, max(1, round(len(sigmas)/10))) > 0 and (i >= min(20, max(1, len(sigmas))/3))):
+            if unconditional_conditioning is None or unconditional_guidance_scale == 1.0 or (i % min(5, max(1, round(len(sigmas)/10))) > 0 and (i >= min(20, max(8, len(sigmas)/3)))):
                 c_out, c_in = [
                     append_dims(tmp, x.ndim) for tmp in cvd.get_scalings(s_i)
                 ]
