884f28aff901cd01bbe7eb902f3e8c2a67407340
Astropulse <61034487+Astropulse@users.noreply.github.com>
2024-03-13 12:09:23 -0400

    Fix resize+pixelate resampling

--- scripts/image_server.py
+++ scripts/image_server.py
@@ -2959,7 +2959,7 @@ async def server(websocket):
                             init_img = decodeImage(values["images"][0])
 
                             # Resize image to output dimensions
-                            image = init_img.resize((values["width"], values["height"]), resample=Image.Resampling.BILINEAR)
+                            image = init_img.resize((values["width"], values["height"]), resample=Image.Resampling.BOX)
 
                             # i2i strength
                             strength = 0.9
@@ -3045,7 +3045,7 @@ async def server(websocket):
                             init_img = decodeImage(values["images"][0])
 
                             # Resize image to output dimensions
-                            image = init_img.resize((values["width"], values["height"]), resample=Image.Resampling.BILINEAR)
+                            image = init_img.resize((values["width"], values["height"]), resample=Image.Resampling.BOX)
 
                             # Blur filter for pixelate
                             image_blur = image.filter(ImageFilter.BoxBlur(2))
